0541263eb5bb0a195c10a8ec09caee93dd8b3814
rostislav <rostmoguchiy@gmail.com>
2020-05-24 21:41:04 +0300

    fully added change top banner logic in admin

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -5,7 +5,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from app.admin import admin
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from app import login_manager
 from jinja2 import TemplateNotFound
@@ -32,9 +32,10 @@ def banners():
 
     if top_banner_form.validate_on_submit():
         filename = "top_banner.jpg"
-
         top_banner = request.files["file"]
-        
+        file_path = os.getcwd() + "/app/site/static/img/" + filename
+        top_banner.save(file_path)
+        flash("Топ банер был изменен")
         return redirect(url_for("admin_blueprint.banners"))
 
     return render_template('index2.html', top_banner_form=top_banner_form)
